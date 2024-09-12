d3509fe76132d1705e87e5750b8901ba6ff5e643
Qurratul Aini <qurratulaini270306@gmail.com>
2024-09-12 16:57:43 +0800

    penjumlahan.py

---
+++ penjumlahan.py
@@ -0,0 +1,7 @@
+def jumlahkan_dua_bilangan():
+    a = float(input("Masukkan bilangan pertama: "))
+    b = float(input("Masukkan bilangan kedua: "))
+    hasil = a + b
+    print("Hasil penjumlahan:", hasil)
+
+jumlahkan_dua_bilangan()
